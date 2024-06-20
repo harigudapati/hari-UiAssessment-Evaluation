91217e4872b439638d388c5417409c38d286d809
Hari Gudapati <harikrishnagudapati1@gmail.com>
2024-06-20 18:32:41 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import fetch from './api/dataService'
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+const calculateRewardPoints = (amountSpent) => {
+  let points = 0
+
+  // Calculate points for dollars spent over $100
+  if (amountSpent > 100) {
+    points = points + (amountSpent - 100) * 2
+  }
+
+  // Calculate points for dollars spent over $50
+  if (amountSpent > 50) {
+    points = points + (amountSpent - 50)
+  }
+
+  return points
 }
 
-export default App;
+const getMonthName = (transactionDate) => {
+  return new Date(transactionDate).getMonth()
+}
+
+function App() {
+  const [transactions, setTransactions] = useState(null)
+
+  const monthlyPoints = {}
+
+  //calculate reward points for each month
+  transactions?.forEach((transaction) => {
+    const points = calculateRewardPoints(transaction?.amt)
+
+    if (!monthlyPoints[months[getMonthName(transaction?.transactionDt)]]) {
+      monthlyPoints[months[getMonthName(transaction?.transactionDt)]] = 0
+    }
+
+    monthlyPoints[months[getMonthName(transaction?.transactionDt)]] += points
+  })
+
+  //calculate total reward points
+  const totalPoints = Object.values(monthlyPoints).reduce(
+    (acc, points) => acc + points,
+    0
+  )
+
+  // let pointsEarnedByCustPerMonth = transactions?.reduce((acc, cur) => {
+  //   let key = cur['month']
+  //   ;(acc[key] ? acc[key] : (acc[key] = null || [])).push(cur)
+  //   return acc
+  // }, [])
+
+  // pointsEarnedByCustPerMonth?
+
+  useEffect(() => {
+    fetch().then((data) => {
+      const newData = data?.map((item) => ({
+        ...item,
+        month: months[getMonthName(item?.transactionDt)],
+      }))
+      setTransactions(newData)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Reward points calculator</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer</th>
+            <th>Amount</th>
+            <th>Month</th>
+            <th>Reward points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions?.map((transaction, index) => (
+            <tr key={transaction?.id}>
+              <td>{transaction?.name}</td>
+              <td>{transaction?.amt}</td>
+              <td>{transaction?.month}</td>
+              <td>{calculateRewardPoints(transaction?.amt)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Total rewards points: {totalPoints}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/api/dataService.js
@@ -0,0 +1,118 @@
+const data = () => {
+  return Promise.resolve([
+    {
+      id: 1,
+      custid: 1,
+      name: 'cust-1',
+      amt: 120,
+      transactionDt: '05-01-2019',
+    },
+    {
+      id: 2,
+      custid: 1,
+      name: 'cust-1',
+      amt: 75,
+      transactionDt: '05-21-2019',
+    },
+    {
+      id: 3,
+      custid: 1,
+      name: 'cust-1',
+      amt: 94,
+      transactionDt: '05-21-2019',
+    },
+    {
+      id: 4,
+      custid: 1,
+      name: 'cust-1',
+      amt: 10,
+      transactionDt: '06-01-2019',
+    },
+    {
+      id: 5,
+      custid: 1,
+      name: 'cust-1',
+      amt: 75,
+      transactionDt: '06-21-2019',
+    },
+    {
+      id: 6,
+      custid: 1,
+      name: 'cust-1',
+      amt: 200,
+      transactionDt: '07-01-2019',
+    },
+    {
+      id: 7,
+      custid: 1,
+      name: 'cust-1',
+      amt: 1,
+      transactionDt: '07-04-2019',
+    },
+    {
+      id: 8,
+      custid: 1,
+      name: 'cust-1',
+      amt: 80,
+      transactionDt: '07-03-2019',
+    },
+    {
+      id: 9,
+      custid: 1,
+      name: 'cust-1',
+      amt: 224,
+      transactionDt: '07-21-2019',
+    },
+    {
+      id: 10,
+      custid: 2,
+      name: 'cust-2',
+      amt: 125,
+      transactionDt: '05-01-2019',
+    },
+    {
+      id: 11,
+      custid: 2,
+      name: 'cust-2',
+      amt: 75,
+      transactionDt: '05-21-2019',
+    },
+    {
+      id: 12,
+      custid: 2,
+      name: 'cust-2',
+      amt: 10,
+      transactionDt: '06-01-2019',
+    },
+    {
+      id: 13,
+      custid: 2,
+      name: 'cust-2',
+      amt: 75,
+      transactionDt: '06-21-2019',
+    },
+    {
+      id: 14,
+      custid: 2,
+      name: 'cust-2',
+      amt: 200,
+      transactionDt: '07-01-2019',
+    },
+    {
+      id: 15,
+      custid: 2,
+      name: 'cust-2',
+      amt: 224,
+      transactionDt: '07-21-2019',
+    },
+    {
+      id: 16,
+      custid: 3,
+      name: 'cust-3',
+      amt: 120,
+      transactionDt: '06-21-2019',
+    },
+  ])
+}
+
+export default data
